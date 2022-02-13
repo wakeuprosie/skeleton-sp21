c437b7d0008389b460defb6d3690cc924ed2559e
Risa <risapark@gmail.com>
2022-02-13 13:50:50 -0800

    Test changes to lab1.

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -7,7 +7,7 @@ public class Collatz {
     public static int nextNumber(int n) {
         if (n  == 128) {
             return 1;
-        } else if (n == 5) {
+        } else if (n == 8) {
             return 3 * n + 1;
         } else {
             return n * 2;
