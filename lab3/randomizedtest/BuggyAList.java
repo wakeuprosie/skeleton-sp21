be2ef7a01b1e44814baf605d5c8c43e80d807975
Risa <risapark@gmail.com>
2022-02-21 17:39:14 -0800

    Completed lab 3.

--- lab3/randomizedtest/BuggyAList.java
+++ lab3/randomizedtest/BuggyAList.java
@@ -59,8 +59,8 @@ public class BuggyAList<Item> {
     /** Deletes item from back of the list and
       * returns deleted item. */
     public Item removeLast() {
-        if ((size < items.length / 4) && (size > 4)) {
-            resize(size / 4);
+        if (size < items.length) {
+            resize(size * 2);
         }
         Item x = getLast();
         items[size - 1] = null;
--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -9,44 +9,67 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE/
-    public static void testThreeAddThreeRemove() {
-        /* run on no resize class */
-        AListNoResizing<Integer> wrong = new AListNoResizing<Integer>();
-        BuggyAList<Integer> right = new BuggyAList<Integer>();
+    @Test
+    public static void randomizedTest() {
+        AListNoResizing<Integer> L = new AListNoResizing<>();
+        BuggyAList<Integer> M = new BuggyAList<>();
 
-        int[] input = {1, 2, 3};
-        Boolean[] output = new Boolean[3];
-        Boolean[] expected = {true, true, true};
-        int index = 0;
-
-        for (int i : input) {
-            wrong.addLast(i);
-            right.addLast(i);
-        }
-
-        int w, r;
-
-        for (int i = 0; i < 2; i += 1) {
-            w = wrong.removeLast();
-            r = right.removeLast();
-            if (w == r) {
-                output[i] = true;
-                index += 1;
-            } else {
-                output[i] = false;
-                index += 1;
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                L.addLast(randVal);
+                M.addLast(randVal);
+            } else if (operationNumber == 1) {
+                // size
+                int size = L.size();
+                int size2 = M.size();
+                assertEquals(size, size2);
+            } else if (operationNumber == 2) {
+                // getLast
+                if (L.size() > 0 &&  M.size() > 0) {
+                    int l = L.getLast();
+                    int m = M.getLast();
+                    assertEquals(l, m);
+                    int size = L.size();
+                    int size2 = M.size();
+                }
+            } else if (operationNumber == 3) {
+                // removeLast
+                if (L.size() > 0 && M.size() > 0) {
+                    L.removeLast();
+                    M.removeLast();
+                    int size = L.size();
+                    int size2 = M.size();
+                }
             }
         }
-
-        for (int i = 0; i < 3; i += 1) {
-            if (output[i] != expected[i]) {
-                System.out.println("Mismatch in position " + i + ", expected " + expected[i] + " but got " + output[i]);
-            }
-        }
-        /* run on resize class */
     }
 
+  @Test
+  public static void testThreeAddThreeRemove() {
+      AListNoResizing<Integer> correct = new AListNoResizing<>();
+      BuggyAList<Integer> broken = new BuggyAList<>();
+
+      correct.addLast(5);
+      correct.addLast(10);
+      correct.addLast(15);
+
+      broken.addLast(5);
+      broken.addLast(10);
+      broken.addLast(15);
+
+      assertEquals(correct.size(), broken.size());
+
+      assertEquals(correct.removeLast(), broken.removeLast());
+      assertEquals(correct.removeLast(), broken.removeLast());
+      assertEquals(correct.removeLast(), broken.removeLast());
+  }
+
     public static void main(String[] args) {
+        randomizedTest();
         testThreeAddThreeRemove();
     }
 }
